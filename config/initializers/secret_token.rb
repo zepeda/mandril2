fcc59769b9fc96275272778a3d1ea90037fa5912
Jesse Ocon <jesseocon@mbp-1102.local>
2013-05-25 18:13:44 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mandril2::Application.config.secret_token = '7f7b69e7fb801faeaa4d514e9eacbc49d2ada1d208b54ff2d867f0b0d6fbef043ac12df1f549a7abee5362594f9d208228b9a0fa86e434a49ae3cc411ec60678'
